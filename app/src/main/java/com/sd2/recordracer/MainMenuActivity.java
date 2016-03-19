054bcb4c379ba0af46ace00515258a5b1114297b
Drew <dwood12@ufl.edu>
2016-03-19 17:30:32 -0400

    adding maps

--- app/src/main/java/com/sd2/recordracer/MainMenuActivity.java
+++ app/src/main/java/com/sd2/recordracer/MainMenuActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -13,7 +14,8 @@ import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainMenuActivity extends Activity {
 
-    private Spinner spinner;
+    private Spinner spinner_exercise;
+    private Spinner spinner_playlist;
     private Button btnSubmit;
 
     @Override
@@ -21,16 +23,28 @@ public class MainMenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        addItemsOnPlaylistSpinner();
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
     }
 
+    //fill spinner with playlist music
+    public void addItemsOnPlaylistSpinner() {
+        spinner_playlist = (Spinner) findViewById(R.id.spinner_playlist);
+        List<String> list = new ArrayList<String>();
+        // add music code here to fill list
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_playlist.setAdapter(dataAdapter);
+    }
+
     public void addListenerOnSpinnerItemSelection() {
-        spinner = (Spinner) findViewById(R.id.exercise_type_spinner);
+        spinner_exercise = (Spinner) findViewById(R.id.exercise_type_spinner);
     }
 
     public void addListenerOnButton() {
-        spinner = (Spinner) findViewById(R.id.exercise_type_spinner);
+        spinner_exercise = (Spinner) findViewById(R.id.exercise_type_spinner);
+        spinner_playlist = (Spinner) findViewById(R.id.spinner_playlist);
         btnSubmit = (Button) findViewById(R.id.button);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
